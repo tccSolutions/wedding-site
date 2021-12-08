f6eac5dc2b98cb3733a9bcaa332f062c4bfa50f3
tccSolutions <tim@tccs.tech>
2021-12-08 12:02:00 -0500

    database update

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ pictures = photos
 
 class RSVP(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100),  nullable=False)
     num_guests = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
