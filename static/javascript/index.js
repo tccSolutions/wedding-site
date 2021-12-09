53d4de63f2bd29b126dea3000559d2884eeef37d
tccSolutions <tim@tccs.tech>
2021-12-09 13:19:08 -0500

    navbar update

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -6,14 +6,20 @@
 
 nav = document.getElementsByTagName('nav')[0]
 registry_field =document.getElementsByClassName("registry")[0]
-document.getElementsByClassName("registry_btn")[0].onclick=function (){
- registry_field.style.visibility = "visible"
-}
-
-registry_field.onmouseleave = function (){
- registry_field.style.visibility = "hidden"
-}
-
-nav.onmouseleave=function (){
- registry_field.style.visibility = "hidden"
-}
+// document.getElementsByClassName("registry_btn")[0].onclick=function (){
+//  registry_field.style.visibility = "visible"
+// }
+//
+// registry_field.onmouseleave = function (){
+//  registry_field.style.visibility = "hidden"
+// }
+//
+// nav.onmouseleave=function (event){
+//  console.log(event)
+//  if (event.screenX > 2280 && event.screenX < 2550){
+//
+//  }else{
+//   registry_field.style.visibility = "hidden"
+//  }
+//
+// }
