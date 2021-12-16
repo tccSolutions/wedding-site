c53bf10a20951f0ec340bc968f561269f8be4973
tccSolutions <tim@tccs.tech>
2021-12-16 09:27:39 -0500

    updated nav bar and pages to make better use of templating

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,16 +1,3 @@
+document.body.style.backgroundImage = "url('../static/images/background.jpg')"
 
 
-
-
-
-
-mobile_nav_icon = document.getElementsByClassName('mobile-nav-icon')[0]
-registry_field =document.getElementsByClassName("registry")[0]
-mobile_dropdown = document.getElementsByClassName("mobile-dropdown")[0]
-mobile_nav_icon.onclick = function (){
- if(mobile_dropdown.style.visibility === "visible") {
-  mobile_dropdown.style.visibility = "hidden";
- }else{
-  mobile_dropdown.style.visibility = "visible";
- }
-}
